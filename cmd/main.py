ec89d5bcb9f0d28597482f1bea7bef25d401aacc
tianyuan <tianyuanyuans@163.com>
2023-04-26 18:51:02 +0800

    feat: main.py logic

--- cmd/main.py
+++ cmd/main.py
@@ -1,4 +1,27 @@
+import os, sys
+sys.path.append(os.getcwd())
+sys.path.append(os.getcwd() + '/protopb/gen/py3/')
+
+print(sys.path)
+
+from protopb.gen.py3.protos import oss_pb2_grpc as oss
+
+import grpc
+from concurrent import futures
+
+class OssService(oss.OssServicer):
+    def Upload(self, request, context):
+        print('upload')
+
+
+def run():
+    server = grpc.Server(futures.ThreadPoolExecutor(max_workers=10))
+    oss.add_OssServicer_to_server(OssService, server)
+
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    print('start')
+    run()
